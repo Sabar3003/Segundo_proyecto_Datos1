2fc6ce9c475a7c01aec9ec43bdac9cbf59d1285a
sabar1002-admin <asbcr1002@gmail.com>
2026-06-08 20:35:07 -0600

    Agrega resultado de camino mínimo en la interfaz

--- src/ui/MainWindow.java
+++ src/ui/MainWindow.java
@@ -165,7 +165,8 @@ public class MainWindow extends Application {
         InfoPanel infoPanel = new InfoPanel(
                 logisticsData,
                 recommendedRoutes,
-                mstResult
+                mstResult,
+                graphPane
         );
 
         BorderPane root = new BorderPane();
